11e154d121baed5703947ac3ca2eeaf047efcfd2
José Pérez <jbperezf@gmail.com>
2020-04-12 20:29:53 +0100

    Update Sun 12 Apr 20:29

---
+++ battle/classes/game.py
@@ -0,0 +1,76 @@
+import random
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Person:
+    def __init__(self, hp, mp, atk, df, magic, items):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.items = items
+        self.actions = ["Attack", "Magic", "Items"]
+
+    def generate_dmg(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    def take_dmg(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
+
+    def heal(self, dmg):
+        self.hp += dmg
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
+
+    def reduce_mp(self, cost):
+        self.mp -= cost
+
+    def choose_action(self):
+        i = 1
+        print(bcolors.OKBLUE + bcolors.BOLD + "ACTIONS" + bcolors.ENDC)
+        for item in self.actions:
+            print(" {}. {}".format(i, item))
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print(bcolors.OKBLUE + bcolors.BOLD + "MAGIC" + bcolors.ENDC)
+        for spell in self.magic:
+            print(" {}. {} (cost: {})".format(i, spell.name, spell.cost))
+            i += 1
+        print("\nEnter 0 to go back.")
+
+    def choose_item(self):
+        i = 1
+        print(bcolors.OKBLUE + bcolors.BOLD + "ITEMS" + bcolors.ENDC)
+        for item in self.items:
+            print(" {}. {}: {}. (x{})".format(i, item["item"].name, item["item"].description, item["quantity"]))
+            i += 1
+        print("\nEnter 0 to go back.")
