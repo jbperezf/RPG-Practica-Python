2a81c8ea195ba4849e58a717349db9d8af7c5af4
José Pérez <jbperezf@gmail.com>
2020-04-15 20:39:03 +0100

    actualización 15

--- battle/classes/game.py
+++ battle/classes/game.py
@@ -13,7 +13,8 @@ class bcolors:
 
 
 class Person:
-    def __init__(self, hp, mp, atk, df, magic, items):
+    def __init__(self, name, hp, mp, atk, df, magic, items):
+        self.name = name
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -54,14 +55,27 @@ class Person:
 
     def choose_action(self):
         i = 1
+        print(bcolors.BOLD + self.name + bcolors.ENDC)
         print(bcolors.OKBLUE + bcolors.BOLD + "ACTIONS" + bcolors.ENDC)
         for item in self.actions:
             print(" {}. {}".format(i, item))
             i += 1
 
+    def choose_target(self, enemies):
+        i = 1
+
+        print(bcolors.OKBLUE + bcolors.BOLD + "\nTARGET" + bcolors.ENDC)
+        for enemy in enemies:
+            if enemy.get_hp() != 0:
+                print(" {}. {}".format(i, enemy.name))
+                i += 1
+
+        enemy_choice = int(input("\nChoose a target: ")) - 1
+        return enemy_choice
+
     def choose_magic(self):
         i = 1
-        print(bcolors.OKBLUE + bcolors.BOLD + "MAGIC" + bcolors.ENDC)
+        print(bcolors.OKBLUE + bcolors.BOLD + "\nMAGIC" + bcolors.ENDC)
         for spell in self.magic:
             print(" {}. {} (cost: {})".format(i, spell.name, spell.cost))
             i += 1
@@ -69,8 +83,68 @@ class Person:
 
     def choose_item(self):
         i = 1
-        print(bcolors.OKBLUE + bcolors.BOLD + "ITEMS" + bcolors.ENDC)
+        print(bcolors.OKBLUE + bcolors.BOLD + "\nITEMS" + bcolors.ENDC)
         for item in self.items:
             print(" {}. {}: {}. (x{})".format(i, item["item"].name, item["item"].description, item["quantity"]))
             i += 1
         print("\nEnter 0 to go back.")
+
+    def get_enemy_stats(self):
+        # Formatting
+        hp_bar = ""
+        hp_ticks = (self.hp / self.maxhp) * 50
+        while hp_ticks > 0:
+            hp_bar += "█"
+            hp_ticks -= 1
+        while len(hp_bar) < 50:
+            hp_bar += " "
+
+        hp_string = str(self.hp) + "/" + str(self.maxhp)
+        spaces_needed_in_hp = 11 - len(hp_string)
+        blank_space = " "
+
+        while len(hp_string) < 11:
+            hp_string = (blank_space * spaces_needed_in_hp) + hp_string
+            spaces_needed_in_hp -= 1
+
+        print("                     __________________________________________________ ")
+        print(bcolors.BOLD + "{}:  {} ".format(self.name, hp_string) + bcolors.ENDC + "|"
+              + bcolors.FAIL + hp_bar + bcolors.ENDC + "|")
+
+    def get_players_stats(self):
+        # Formatting the HP and MP bars
+        hp_bar = ""
+        hp_ticks = (self.hp / self.maxhp) * 25
+        while hp_ticks > 0:
+            hp_bar += "█"
+            hp_ticks -= 1
+        while len(hp_bar) < 25:
+            hp_bar += " "
+
+        mp_bar = ""
+        mp_ticks = (self.mp / self.maxmp) * 10
+        while mp_ticks > 0:
+            mp_bar += "█"
+            mp_ticks -= 1
+        while len(mp_bar) < 10:
+            mp_bar += " "
+
+        hp_string = str(self.hp) + "/" + str(self.maxhp)
+        spaces_needed_in_hp = 9 - len(hp_string)
+        mp_string = str(self.mp) + "/" + str(self.maxmp)
+        spaces_needed_in_mp = 7 - len(mp_string)
+        blank_space = " "
+
+        while len(hp_string) < 9:
+            hp_string = (blank_space * spaces_needed_in_hp) + hp_string
+            spaces_needed_in_hp -= 1
+
+        while len(mp_string) < 7:
+            mp_string = (blank_space * spaces_needed_in_mp) + mp_string
+            spaces_needed_in_mp -= 1
+
+        print("                     _________________________               __________ ")
+        print(bcolors.BOLD + "{}:    {}".format(self.name, hp_string) + bcolors.ENDC + " |"
+              + bcolors.OKGREEN + hp_bar + bcolors.ENDC + "|" + bcolors.BOLD +
+              "     {} ".format(mp_string) + bcolors.ENDC + "|" + bcolors.OKBLUE + mp_bar
+              + bcolors.ENDC + "|")
